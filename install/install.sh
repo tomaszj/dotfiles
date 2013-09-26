c193500dfd93512addb3fea804c620d370a1c246
Tomasz Janeczko <janecztom@gmail.com>
2013-09-26 22:27:54 +0200

    Renamed .vimrc to vimrc for better visibility. Suppressed pushd and popd calls

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Script installing TJ's dotfiles in your home folder
 
-pushd .
+echo "Installing TJ's dotfiles..."
+
+pushd . 1> /dev/null
 
 # Find the directory that this script is running from
 INSTALL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -10,6 +12,9 @@ cd $INSTALL_SCRIPT_DIR/..
 WORKING_DIR=$(pwd)
 
 # Link to vim config files
-ln -nfs $WORKING_DIR/vim/.vimrc ~/.vimrc
+ln -nfs $WORKING_DIR/vim/vimrc ~/.vimrc
 
-popd
+popd 1> /dev/null
+
+echo "Done!"
+
