730cd5f277f276a89351103ed1ff700b5a536567
Tomasz Janeczko <janecztom@gmail.com>
2013-09-30 00:41:22 +0200

    Added script for fetching zprezto when needed

--- install/install.sh
+++ install/install.sh
@@ -9,7 +9,12 @@ pushd . 1> /dev/null
 INSTALL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )"
 cd $INSTALL_SCRIPT_DIR/..
 
-WORKING_DIR=$(pwd)
+WORKING_DIR="$(pwd)"
+
+if find $WORKING_DIR/zsh/zprezto -maxdepth 0 -empty | read v; then 
+  echo "Custom zprezto not present, fetching the default one"
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git $WORKING_DIR/zsh/zprezto 
+fi
 
 # Link to vim config files
 ln -nfs $WORKING_DIR/vim/vimrc $HOME/.vimrc
