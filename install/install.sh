8fb045610665d1bd6dcdda5c25748d7d854f2ba9
Tomasz Janeczko <janecztom@gmail.com>
2013-09-29 23:48:53 +0200

    Added zprezto to handle zsh config

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 # Script installing TJ's dotfiles in your home folder
 
 echo "Installing TJ's dotfiles..."
@@ -6,16 +6,19 @@ echo "Installing TJ's dotfiles..."
 pushd . 1> /dev/null
 
 # Find the directory that this script is running from
-INSTALL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INSTALL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )"
 cd $INSTALL_SCRIPT_DIR/..
 
 WORKING_DIR=$(pwd)
 
 # Link to vim config files
-ln -nfs $WORKING_DIR/vim/vimrc ~/.vimrc
+ln -nfs $WORKING_DIR/vim/vimrc $HOME/.vimrc
 
-# Link to zsh config files
-ln -nfs $WORKING_DIR/zsh/zshrc ~/.zshrc
+# Link to zsh config files using zprezto
+setopt EXTENDED_GLOB
+for rcfile in $WORKING_DIR/zsh/zprezto/runcoms/^README.md(.N); do
+  ln -nfs "$rcfile" "$HOME/.${rcfile:t}"
+done
 
 popd 1> /dev/null
 
