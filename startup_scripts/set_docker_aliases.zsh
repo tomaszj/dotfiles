812261e013c26a5dbdf1caec88b7178720fb7bb4
Tomasz Janeczko <t.janeczko@kainos.com>
2015-03-31 22:57:43 +0100

    Removed docker aliases.

--- startup_scripts/set_docker_aliases.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Script setting up tmux alias
-
-alias docka_start="boot2docker start && $(boot2docker shellinit)"
-alias docka_shell="$(boot2docker shellinit)"
-alias docka_ip="boot2docker ip"
-
